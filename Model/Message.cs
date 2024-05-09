fc9afdabef7d421bfd2de58172792b159fe93744
79532 <79532@DESKTOP-7VRIOGU>
2024-05-09 23:07:14 +1000

    123

--- Model/Message.cs
+++ Model/Message.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Kyrsovau_Rabota
 {
-    public class Message
+    public class Message //
     {
         public int id {  get; set; }
         public string ystroy { get; set; }
